787ae23d09c0e0804b3eccc702ff4e4bb322a746
María <mcuervo@correo.unimet.edu.ve>
2024-03-09 00:42:20 -0400

    Primer commit: revisar por que no se ve

--- Micro2/src/main.tsx
+++ Micro2/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import ProductDetail from './pages/ClubDetail.tsx'
+import ClubPage from './pages/ClubPages.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <Routes>
+        <Route path="/" element={<App />} />
+        <Route path='/products' element={<ClubPage />}></Route>
+        <Route path="/product/:id" element={<ProductDetail />} />
+    </Routes>
+  </Router>,
   </React.StrictMode>,
 )
